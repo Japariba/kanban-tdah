074c821bc4f1a4e141d6a401646ad085b401e761
Gustavo <gideriha@yahoo.com.br>
2026-02-18 18:40:53 -0300

    fix: disable prerender on login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,6 +2,7 @@
 
 console.log(process.env.NEXT_PUBLIC_SUPABASE_URL)
 
+export const dynamic = 'force-dynamic'
 
 import { useState } from 'react'
 import { getSupabaseClient } from '@/lib/supabaseClient'
